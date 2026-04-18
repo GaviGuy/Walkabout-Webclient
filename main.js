c27d49a51229df75b278be55e171b696eea706a6
GaviGuy <gaviguy711@gmail.com>
2026-04-18 02:27:44 -0600

    installing skeleton (part 1)

--- main.js
+++ main.js
@@ -1,16 +1,76 @@
-import { Client } from "node_modules/archipelago.js";
+import { Client } from "https://unpkg.com/archipelago.js/dist/archipelago.min.js";
 
 const client = new Client();
 
+
 console.log("gmorning");
+document.getElementById("login-button").addEventListener("click", () => {handleLoginButton()});
+document.getElementById("chat-input").addEventListener("keyup", (e) => {
+    if(e.key === "Enter" || e.keyCode === 13) {
+        let elem = document.getElementById("chat-input");
+        client.messages.say(elem.value);
+        elem.value = "";
+    }
+});
+document.getElementById("top-disconnect").addEventListener("click", () => {disconnect()});
+
+parseCourseInfo();
 
 client.messages.on("message", (content) => {
     console.log(content);
 });
 
-function login(address, player) {
-    client.login(address, player)
-        .then(() => console.log("Connected!"))
-        .catch(console.error);
+function handleLoginButton() {
+    let address = document.getElementById("input-address").value;
+    let username = document.getElementById("input-username").value;
+    let password = document.getElementById("input-password").value;
+
+    if(!address || !username) return;
+
+    if(!password) login(address, username);
+}
+
+function updateTopBar(style, message) {
+    const classes = ["disconnected-0", "connecting", "connected", "disconnected-1"];
+    document.getElementById("connection-info").classList = [`${classes[style]}`];
+    document.getElementById("top-text").innerText = message;
+}
+
+function disconnect() {
+    client.login().catch(()=>{});
+    clearCourseSelect();
+    updateTopBar(0, "Disconnected");
+    //update other relevant places: chat input
+}
+
+function clearCourseSelect() {
+    document.getElementById("course-select-list").innerHTML = "";
+}
+
+function generateCourseSelect() {
+    clearCourseSelect();
+    let coursesElem = document.getElementById("course-select-list");
+    for(let i in courseInfo) {
+        let newElem = document.createElement("div");
+        newElem.classList.add("course-tile");
+        newElem.innerText = `${i} (${courseInfo[i].abbreviation})`;
+        coursesElem.appendChild(newElem);
+    }
+}
+
+function login(address, username) {
+    disconnect();
+    updateTopBar(1, "Connecting...")
+    console.log("logging in...");
+    client.login(address, username)
+        .then((slotData) => {
+            console.log(slotData);
+            updateTopBar(2, `Connected to ${address} as ${username}`);
+            generateCourseSelect();
+        })
+        .catch((e) => {
+            console.error(e);
+            updateTopBar(3, `Failed to connect`);
+        });
 }
 
---
+++ scripts/parseCourseInfo.js
@@ -0,0 +1,38 @@
+let courseInfo = {};
+
+async function parseCourseInfo() {
+    const filePath = "data/courseinfo.json";
+    fetch(filePath).then((e) => {
+        if(!e.ok) throw new Error("wawa");
+        return e.json();
+    }).then((json) => {
+        for(let i in json) {
+            courseInfo[i] = {};
+            courseInfo[i].abbreviation = json[i][0];
+
+            courseInfo[i].easyPars = [];
+            for(let j = 1; j < 21; j++)
+                courseInfo[i].easyPars.push(json[i][j]);
+            
+            courseInfo[i].hardPars = [];
+            for(let j = 21; j < 39; j++)
+                courseInfo[i].hardPars.push(json[i][j]);
+
+            courseInfo[i].easySlingshot = [];
+            if(json[i].easyslingshot)
+                courseInfo.easySlingshot = json[i][39];
+
+            courseInfo[i].hardSlingshot = [];
+            if(json[i].hardslingshot)
+                courseInfo.hardSlingshot = json[i][40];
+
+            courseInfo[i].foxhuntBranchCount = json[i][41];
+            courseInfo[i].mainHuntClues = json[i][42];
+            courseInfo[i].foxhuntBranchCount = json[i][43];
+        }
+    }).catch((e) => {
+        console.error(e)   
+    });
+}
+
+parseCourseInfo();
